1108fa9b028ad0522ba1bbc478f6bfd20b11353b
Arthur Milchior <Arthur@Milchior.fr>
2020-02-19 19:46:20 -0800

    loop over width-height

--- __init__.py
+++ __init__.py
@@ -16,15 +16,12 @@ def setBrowserResizeImage(self):
     js = "\n".join(str_from_file_name(f"{file}.js") for file in js)
     add_style_in_web(self, css)
     self.web.eval(js)
-    if getUserOption("apply maximum height when not resizing", False):
-        max_height = getUserOption("max-height", "200px")
-        add_style_in_web(
-            self, f"""#fields img {{max-height: {max_height} }}""")
-        self.web.eval(f"""max_height = "{max_height}";""")
-    if getUserOption("apply maximum width when not resizing", False):
-        max_width = getUserOption("max-width", "200px")
-        add_style_in_web(self, f"""#fields img {{max-width: {max_width} }}""")
-        self.web.eval(f"""max_width = "{max_width}";""")
+    for direction in ["width", "height"]:
+        if getUserOption(f"apply maximum {direction} when not resizing", False):
+            limit = getUserOption(f"max-{direction}", "200px")
+            add_style_in_web(
+                self, f"""#fields img {{max-{direction}: {limit} }}""")
+            self.web.eval(f"""max_{direction} = "{limit}";""")
     self.web.eval(
         f"""preserve_ratio={json.dumps(getUserOption("preserve ratio while resizing", True))}""")
 
