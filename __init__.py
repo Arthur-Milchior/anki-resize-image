97efc0802e1ef4ba5b9b12e00a6d4de1ce5d8140
Arthur Milchior <Arthur@Milchior.fr>
2020-02-19 19:56:24 -0800

    loopping over min/max

--- __init__.py
+++ __init__.py
@@ -16,14 +16,11 @@ def setBrowserResizeImage(self):
     js = "\n".join(str_from_file_name(f"{file}.js") for file in js)
     add_style_in_web(self, css)
     self.web.eval(js)
-    for direction in ["width", "height"]:
-        if getUserOption(f"apply maximum {direction} when not resizing", False):
-            limit = getUserOption(f"max-{direction}", "200px")
-            add_style_in_web(
-                self, f"""#fields img {{max-{direction}: {limit} }}""")
-            self.web.eval(f"""max_{direction} = "{limit}";""")
-    self.web.eval(
-        f"""preserve_ratio={json.dumps(getUserOption("preserve ratio while resizing", True))}""")
-
-
+    for m, default in [("min", "10px"), ("max", "200px")]:
+        for direction in ["width", "height"]:
+            if getUserOption(f"apply {m}imum {direction} when not resizing", False):
+                limit = getUserOption(f"{m}-{direction}", default)
+                add_style_in_web(self, f"""#fields img {{{m}-{direction}: {limit} }}""")
+                self.web.eval(f"""{m}_{direction} = "{limit}";""")
+    self.web.eval(f"""preserve_ratio={json.dumps(getUserOption("preserve ratio while resizing", True))}""")
 Editor.setupWeb = wrap(Editor.setupWeb, setBrowserResizeImage)
